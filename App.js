9d3c72f468bc98948e99704c285247301924f3fa
Balazs Danyi <danyi.balage@gmail.com>
2020-11-07 17:40:12 +0000

    finished app

--- App.js
+++ App.js
@@ -1,16 +1,81 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { 
+  StyleSheet, 
+  Image, 
+  Text, 
+  View, 
+  TextInput, 
+  Button 
+} from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('');
+  const [showedName, setShowedName] = useState('');
+
+  const onButtonPress = () => {
+    if (name) {
+      setShowedName(name);
+      setName('');
+    }  
+  }
+
   return (
-    <View style={styles.container}>
-      <Text></Text>
+    <View style={styles.mainContainer}>
+      <Image 
+        style={styles.image} 
+        source={require('./assets/logo.png')} 
+        resizeMode='contain'
+      /> 
+      <View>
+        <Text style={styles.welcomeText}>
+          Üdv, {showedName}!
+        </Text>
+      </View> 
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input} 
+          placeholder="Név" 
+          value={name}
+          onChangeText={(enteredText) => setName(enteredText)}
+        />
+        <View style={styles.buttonContainer}>  
+          <Button
+            title="Küldés"
+            color="#3797f1"
+            onPress={onButtonPress}
+          />
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    
+  mainContainer: {
+    alignItems: 'center',
+    padding: 30
+  },
+  image: {
+    width: '100%'
+  },
+  welcomeText: {
+    fontSize: 20,
+    marginVertical: 30
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  input: {
+    flex: 4,
+    paddingHorizontal: 10,
+    backgroundColor: '#fbfbfb',
+    borderColor: '#e0e0e0',
+    borderWidth: 1,
+    borderRadius: 15
+  },
+  buttonContainer: {
+    flex: 2,
+    marginLeft: 10
   }
 });
---
+++ README.md
@@ -0,0 +1,34 @@
+# React Native tesztfeladat 1
+
+## Feladat Leírás
+
+Telepítsd a react native-ot és készítsd el a következő feladatot. A feladat egy olyan képernyő létrehozása, amelyen 4 komponens található az alábbi sorrendben.
+	- Kép
+	- Szöveg
+	- Szöveg beviteli mező és mellette egy gomb
+
+A kép a Medio logó legyen közvetlen eléréssel, ne távolival.
+
+A szöveg a képernyő betöltésekor a következő legyen: Üdv, !
+
+Szöveg beviteli mező placeholder: Név
+
+Gomb felirat: Küldés
+
+A gombra kattintva a szöveg legyen:
+
+Üdv [Szöveg beviteli mező értéke]!
+
+## Fejlesztöi környezet
+
+Expo CLI
+
+## Futtatás
+
+1. npm install
+2. npm start
+
+## Tesztek
+
+iOS: iPhone 7 (fizikalis eszköz)
+Android: Google Pixel 3A (Android Studio)
